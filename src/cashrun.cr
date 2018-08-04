4ff6a91bab92adeee62f76d561e4440250c33723
Will Lewis <will@wflewis.com>
2018-08-03 21:47:10 -0400

    Clean up code using macros

--- src/cashrun.cr
+++ src/cashrun.cr
@@ -4,10 +4,16 @@ module Cashrun
   VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
 end
 
+macro vputs(string)
+  if config.verbose
+    STDERR.puts {{string}}
+  end
+end
+
 config, script_name, remaining_args = Cashrun::CLI.parse
 config.parse_file!
-STDERR.puts "Config: #{config}" if config.verbose
-STDERR.puts "Running #{script_name}" if config.verbose
+vputs "Config: #{config}"
+vputs "Running #{script_name}"
 
 unless File.exists?(script_name)
   STDERR.puts "Origin script does not exist."
@@ -15,18 +21,18 @@ unless File.exists?(script_name)
 end
 
 hexdigest = config.digest.class.hexdigest(File.read(script_name))
-STDERR.puts "Hex Digest: #{hexdigest}" if config.verbose
+vputs "Hex Digest: #{hexdigest}"
 
 cached_name = File.expand_path(File.join(config.cache_directory, hexdigest))
-STDERR.puts "Cached Name: #{cached_name}" if config.verbose
+vputs "Cached Name: #{cached_name}"
 
 unless Dir.exists?(File.expand_path(config.cache_directory))
-  STDERR.puts "Cache Directory does not exist." if config.verbose
+  vputs "Cache Directory does not exist."
   Dir.mkdir_p(File.expand_path(config.cache_directory))
 end
 
 unless File.exists?(cached_name)
-  STDERR.puts "Cached executable does not exist." if config.verbose
+  vputs "Cached executable does not exist."
 
   real_script_location = File.real_path(File.expand_path(script_name))
 
@@ -41,9 +47,9 @@ unless File.exists?(cached_name)
     args << "--release"
   end
 
-  STDERR.puts "running `crystal #{args.join(" ")}` after chdir: `#{real_script_location}`" if config.verbose
+  vputs "running `crystal #{args.join(" ")}` after chdir: `#{real_script_location}`"
   Process.run(command: "crystal", args: args, chdir: real_script_location)
 end
 
-STDERR.puts "running `#{cached_name} #{remaining_args.join(" ")}`" if config.verbose
+vputs "running `#{cached_name} #{remaining_args.join(" ")}`"
 Process.exec(command: cached_name, args: remaining_args)
